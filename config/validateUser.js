96a7122f5b3008ff6c1fd11a62321046c3df2e2f
andhac <andhac84@yahoo.com>
2022-01-31 18:44:52 +0530

    ..

--- config/validateUser.js
+++
@@ -1,5 +0,0 @@
-export default  (req,res) => {
-    if(!req.session.passport.user._doc._id.equals(req.params._id)){
-        return res.status(401).json({eror: "THIS IS NOT YOUR ACCOUNT"})
-    }
-} 
